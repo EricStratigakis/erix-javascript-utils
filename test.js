cba2bef8d684ecbc395a74367432f938ada7d5cf
Eric Stratigakis <enstrati@uwaterloo.ca>
2021-09-07 00:16:16 -0400

    removed false faliure

--- test.js
+++ test.js
@@ -3,7 +3,3 @@ const { removeHTML } = require("./src/index");
 test("strips <p>Hi there</p>", () => {
   expect(removeHTML("<p>Hi there</p>")).toBe("Hi there");
 });
-
-test("False Faliure", () => {
-  expect(removeHTML("<p>Hi there</p>")).toBe("False Faliure");
-});
