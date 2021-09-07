561f989ff2ea19a202c74505d563c671cae5c7f0
Eric Stratigakis <enstrati@uwaterloo.ca>
2021-09-07 19:43:17 -0400

    added testing and successfull deploy with fail

--- test.js
+++ test.js
@@ -3,3 +3,7 @@ const { removeHTML } = require("./package/index");
 test("strips <p>Hi there</p>", () => {
   expect(removeHTML("<p>Hi there</p>")).toBe("Hi there");
 });
+
+test("strips <p>Hi there</p>", () => {
+  expect(removeHTML("<p>Hi there</p>")).toBe("His there");
+});
