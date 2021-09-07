e9fe6b62b239ea52f2cb6a58238949d4a4fe33c5
Eric Stratigakis <enstrati@uwaterloo.ca>
2021-09-06 23:24:23 -0400

    updated verison added ci

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
-export const removeHTML = (str) => {
+const removeHTML = (str) => {
   if (str === null || str === "") return false;
   else str = str.toString();
   return str.replace(/(<([^>]+)>)/gi, "");
+};
+
+module.exports = {
+  removeHTML,
 };
