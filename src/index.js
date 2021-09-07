83bd1eb33499c87a0fd7f00a2feae3002a2845b9
Eric Stratigakis <enstrati@uwaterloo.ca>
2021-09-07 01:37:41 -0400

    cli

--- src/index.js
+++
@@ -1,9 +0,0 @@
-const removeHTML = (str) => {
-  if (str === null || str === "") return false;
-  else str = str.toString();
-  return str.replace(/(<([^>]+)>)/gi, "");
-};
-
-module.exports = {
-  removeHTML,
-};
